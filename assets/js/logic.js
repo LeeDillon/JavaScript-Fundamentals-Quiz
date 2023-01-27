d54d9dc11add93160fd66d92bc0d4763923357a9
Lee-Dillon <lee.dillon1@outlook.com>
2023-01-27 16:48:09 +0000

    struggling with question number generation

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,11 +2,13 @@
 const startSection = $('#start-screen');
 const startBtnEl = $('#start');
 const questionsEl = $('#questions');
+const questionnumberEl = $('#question-number');
 const questionTitleEl = $('#question-title');
 const choicesEl = $('#choices');
 const finalScoreEl = $('#final-score');
 const feedbackEl = $('#feedback');
 
+var questionNumber = 0;
 
 
 // Function for getting a random element from an array
@@ -20,18 +22,21 @@ function getRandom(arr) {
 function loadQuestion() {
     questionsEl.css('display', 'block');
     startSection.css('display', 'none');
-    // for (i = 0; i < 10; i++) {
+
     var questionIndex = Math.floor(Math.random() * questionArray.length);
     var randomQuestionArray = questionArray[questionIndex];
-    questionTitleEl.text(randomQuestionArray[0]);
+
 
 
     for (i = 2; i < randomQuestionArray.length; i++) {
+        questionNumber++;
+        questionnumberEl.text(questionNumber);
+        questionTitleEl.text('Question No. ' + questionNumber + ": " + randomQuestionArray[0]);
         var answerBtnEl = $('<button>');
         answerBtnEl.attr('id', 'answer');
         answerBtnEl.text(randomQuestionArray[i]);
         answerBtnEl.addClass('answer-button');
-        answerBtnEl.on('click', loadQuestion);
+        answerBtnEl.on('click', clearAnswers);
         choicesEl.append(answerBtnEl);
     }
     questionArray.splice(questionIndex, 1);
@@ -40,11 +45,10 @@ function loadQuestion() {
 startBtnEl.on('click', loadQuestion);
 
 
-// Function for when an answer is clicked
-// answerBtnEl.on('click', function () {
-//     var newPassword = passwordGenerator(15);
-//     passwordDisplayEl.text(newPassword);
-//     loadQuestion();
-// });
+// Function for clearing answer buttons and loading next question
+function clearAnswers() {
+    choicesEl.empty();
+    loadQuestion();
+}
 
 
