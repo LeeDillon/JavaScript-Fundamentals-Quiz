05da79d4b8ba48589f4d1a6a1b200dc770319a07
Lee-Dillon <lee.dillon1@outlook.com>
2023-01-27 16:20:23 +0000

    managed to get questions to generate after an answer is selected

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,13 @@
 // Assigning elements i need to variables
-var questionTitleEl = $('#question-title');
-var choicesEl = $('#choices');
-var finalScoreEl = $('#final-score');
-var feedbackEl = $('#feedback');
-var startBtnEl = $('#start');
+const startSection = $('#start-screen');
+const startBtnEl = $('#start');
+const questionsEl = $('#questions');
+const questionTitleEl = $('#question-title');
+const choicesEl = $('#choices');
+const finalScoreEl = $('#final-score');
+const feedbackEl = $('#feedback');
+
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -14,24 +18,27 @@ function getRandom(arr) {
 
 // Function to select 10 questions at random and populate the quiz accordingly
 function loadQuestion() {
-    questionTitleEl.attr('class', '');
+    questionsEl.css('display', 'block');
+    startSection.css('display', 'none');
     // for (i = 0; i < 10; i++) {
     var questionIndex = Math.floor(Math.random() * questionArray.length);
     var randomQuestionArray = questionArray[questionIndex];
-    questionTitleEl.text = randomQuestionArray[0];
+    questionTitleEl.text(randomQuestionArray[0]);
+
 
     for (i = 2; i < randomQuestionArray.length; i++) {
         var answerBtnEl = $('<button>');
         answerBtnEl.attr('id', 'answer');
         answerBtnEl.text(randomQuestionArray[i]);
+        answerBtnEl.addClass('answer-button');
+        answerBtnEl.on('click', loadQuestion);
         choicesEl.append(answerBtnEl);
     }
     questionArray.splice(questionIndex, 1);
 }
 
-startBtnEl.on('click', function () {
-    loadQuestion();
-});
+startBtnEl.on('click', loadQuestion);
+
 
 // Function for when an answer is clicked
 // answerBtnEl.on('click', function () {
