cf6ab5bf1a9a00a13b894e299d7e36c0589ad898
Lee-Dillon <lee.dillon1@outlook.com>
2023-01-31 23:10:29 +0000

    fixed issue with clear button, ready to submit

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,12 +1,13 @@
 // Assigning highscores element to variable
 const highscoresEl = $('#highscores');
+const clearButtonEl = $('#clear');
 
 // Function to get scores array from local storage then iterate through this and create a new list element for each object. This list item then shows the details we want from each object.
 function displayScores() {
     scores = JSON.parse(localStorage.getItem("leaderboard") || "[]");
     scores.forEach(element => {
         var userScoreEl = $('<li>');
-        userScoreEl.text(element.initials + element.finalScore + element.finalTime);
+        userScoreEl.text("Initials: " + element.initials + " Final Score: " + element.finalScore + " Time Remaining: " + element.finalTime);
         highscoresEl.append(userScoreEl);
     });
 }
@@ -14,8 +15,12 @@ function displayScores() {
 displayScores();
 
 // Function to clear screen of scores and delete scores array from local storage
+
 function clearScores() {
     highscoresEl.empty();
     localStorage.clear();
 }
+
+// Add event listener to clear button that links to function
+clearButtonEl.on('click', clearScores);
 
